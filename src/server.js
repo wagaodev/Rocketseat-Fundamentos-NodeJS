191f3a64b17d93e4c5a1678807592ef4ef1583cd
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2023-12-05 02:02:06 -0300

    feat: create database to save the users in our database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
 import "dotenv/config";
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -10,17 +12,20 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+
     return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
-    const id = Math.floor(Math.random() * 100);
-    users.push({
-      id,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    });
+    };
+
+    database.insert("users", user);
 
     return res.writeHead(201).end();
   }
@@ -29,5 +34,5 @@ const server = http.createServer(async (req, res) => {
 });
 
 server.listen(process.env.PORT, () => {
-  console.log("Está rodando na porta", process.env.PORT);
+  console.log("Está rodando na porta =>", process.env.PORT);
 });
